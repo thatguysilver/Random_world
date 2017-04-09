5e8eecb3854136c0d2ca1fd73363850d6a4f8e75
Adam Silver <adampaulsilver@gmail.com>
2017-04-09 14:43:55 -0400

    To do: return string instead of fx

--- creatures_class.py
+++ creatures_class.py
@@ -2,21 +2,21 @@ from randomizer import *
 
 class creature:
     '''Everything that breathes is one of these.'''
-    def num_of_legs(self, num_of_legs):
+    def num_of_legs(self, num_of_legs = ):
         self.num_of_legs = num_of_legs
         num_of_legs = random.randint(1, 100)
         if num_of_legs <=35:
-            return "2"
-        elif num_of_legs > 35 and x <= 70:
-            return "4"
-        elif num_of_legs > 70 and x <= 95:
-            return "8"
-        else:
-            return "6"
+            return 2
+        if num_of_legs <=70 and > 35:
+            return 4
+        if num_of_legs <=95 and > 70:
+            return 8
+        if num_of_legs <=100 and > 95:
+            return 6
 
 
 class person(creature):
-    #default person class; to be a subclass of creature
+    '''default person class; to be a subclass of creature
     def __init__(self, person_name):
         self.person_name = person_name
     number_of_legs = 2 #class-level attribute
--- main.py
+++ main.py
@@ -1,17 +1,27 @@
 from random import *
 from randomizer import name_creator
-from person_class import *
+from person_class import creature, person
 
 
 population = 10 #randrange(100, 1000)
-def pop_generator():
+def creature_pop_generator():
+    '''generates the number and
+    types of creatures'''
+    for x in range(0,9): #range #of creature types
+        x = creature()
+        print(x.num_of_legs)
+
+
+
+def pop_generator(): #Generates people by calling the person_class
     for x in range(0, population):
-        x = name_creator()
-        name = person(x)
-        print(name.person_name)
-        print(name.number_of_legs)
+        x = name_creator() #sets x equal to a string of gibberish.
+        name = person(x) #establishes a variable here.
+        print(name.person_name) #prints the name.
+        print(name.number_of_legs) #prints a class-level variable for SnGs
 
 pop_generator()
+creature_pop_generator()
 #Barebones foundation for future project: Each generated name will
     #be assigned to a class as a variable and a list of attributes will
     #be randomly generated for it.
