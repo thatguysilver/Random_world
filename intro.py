022082ebc34f4cc3f8632ad2357a8da6dc6ce3be
Adam Silver <adampaulsilver@gmail.com>
2017-04-15 09:52:33 -0400

    created a git intro file; expecting a lot of hard resets over the course of the day as I try to populate it.

---
+++ intro.py
@@ -0,0 +1,7 @@
+'''
+This will be the intro file; I'm going to begin
+the long and irritating process of moving most of
+the functions in main over here. Crossing my fingers
+that I don't break this thing in the process.
+Thank God for git.
+'''
