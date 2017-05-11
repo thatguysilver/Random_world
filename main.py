16ccd96336f40b713bfc242ad3cd5532079056b4
Adam Silver <adampaulsilver@gmail.com>
2017-05-11 17:23:55 -0400

    removed dependency on creatures_class.py in main.py

--- main.py
+++ main.py
@@ -2,7 +2,6 @@
 from random import * #apparently this format is bad. Whoops.
 from sys import argv #necessary for writing to file
 from randomizer import name_creator
-#from creatures_class import creatures_text
 from magic_system import magic_text
 import os
 
